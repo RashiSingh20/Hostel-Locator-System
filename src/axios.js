17117380cbf422bb33fa8312c429da48e91c5a97
RashiSingh20 <rashisingh2620@gmail.com>
2021-06-22 18:50:16 +0530

    Added heroku backend link in axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api"
+    baseURL: "https://hostel-locator-system.herokuapp.com/api"
 });
 
 export default instance;
